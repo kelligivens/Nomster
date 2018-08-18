9b15a86d0cd3748de8a5888a297117439f064d15
Kelli Givens <kelligivens@live.co.za>
2018-08-18 12:18:51 -0400

    Add validation of user input

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
   end
   
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
   
   def show
@@ -29,8 +33,15 @@ end
   def update
     @place = Place.find(params[:id])
     
-   if @place.user != current_user
-    return render plain: 'Not Allowed', status: :forbidden
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
+  @place.update_attributes(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
   end
 end
   
