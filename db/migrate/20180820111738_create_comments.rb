67dae2a24d7e52e8b46b6260145da1350fb9cbda
Kelli Givens <kelligivens@live.co.za>
2018-08-26 12:26:13 -0400

    Revised comment form

--- db/migrate/20180820111738_create_comments.rb
+++ db/migrate/20180820111738_create_comments.rb
@@ -7,6 +7,7 @@ class CreateComments < ActiveRecord::Migration[5.0]
       t.integer :place_id
       t.timestamps
     end
+    
     add_index :comments, [:user_id, :place_id]
     add_index :comments, :place_id
   end
