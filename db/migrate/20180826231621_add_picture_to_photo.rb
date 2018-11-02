939d6e921112d09cd1cc265637d744ec56fa1556
kelligivens <kelligivens@live.co.za>
2018-11-01 23:43:22 -0400

    Update Loofah gem

--- db/migrate/20180826231621_add_picture_to_photo.rb
+++ db/migrate/20180826231621_add_picture_to_photo.rb
@@ -1,5 +1,5 @@
-class AddPictureToPhoto < ActiveRecord::Migration[5.0]
+class AddPictureToPhotos < ActiveRecord::Migration[5.0]
   def change
-    add_column :photos, :picture, :string
+       add_column :photos, :picture, :string
   end
 end
